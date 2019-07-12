aba56d3a711c1a822e83826c13794233bdc8e77e
coalchan <coalchan@gmail.com>
2019-07-12 17:51:27 +0800

    add: scripts

---
+++ publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+msg=$1
+if [[ -z "$msg" ]]; then
+  echo "提交信息不能为空"
+  exit -1
+fi
+
+git checkout gh-pages
+
+echo "---------------------------commit static..."
+cp -rf _book/* .
+git add .
+git commit -m "$msg"
+
+echo "---------------------------push static..."
+git push origin gh-pages
+
+git checkout master
